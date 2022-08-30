1236630387dbda65abe3598e3eafb3f8f8ac849d
Mike Rachman <mikerachman1@gmail.com>
2022-08-30 13:27:11 -0400

    done with lesson

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :link, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
+  has_many :comments
+end
